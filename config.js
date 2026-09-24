57be453ce6cc7e72a0b8b20ec14edbca86f170bf
unexpectedItem <jma_uk@mac.com>
2026-09-24 21:19:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // These values are safe to publish in a browser app when Supabase Row Level Security is enabled.
 // NEVER put a Supabase secret/service-role key or your tracker password in this file.
 window.RECOVERY_TRACKER_CONFIG = {
-  supabaseUrl: 'https://itqpxmovvrgtibqzhjaz.supabase.co/rest/v1/',
+  supabaseUrl: 'https://itqpxmovvrgtibqzhjaz.supabase.co',
   publishableKey: 'sb_publishable_iZR-JqIg5p6DG7GLBRhd4w_lYr0CaQk',
   loginEmail: 'jma_uk@me.com'
 };
